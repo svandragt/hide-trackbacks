1db094e705daaba9ab9c01eaeb047fc984e07650
Sander van Dragt <sander@vandragt.com>
2025-11-28 09:49:28 +0000

    Bump version to 1.1.7

--- hide-trackbacks.php
+++ hide-trackbacks.php
@@ -4,7 +4,7 @@
 Plugin Name: Hide Trackbacks
 Plugin URI: http://wp.me/p1vXha-4u
 Description: Prevents trackbacks and pingbacks from showing up as comments on your posts.
-Version: 1.1.5
+Version: 1.1.7
 Author: Sander van Dragt
 Author URI: https://vandragt.com
 License: GPL2
